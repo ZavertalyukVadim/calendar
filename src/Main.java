a70985c6cc4b91f2fe3fd260e7b378306041c095
Вадимка <zavertal.v@gmail.com>
2016-07-16 18:35:20 +0300

    create project.

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Вадимка on 16.07.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println((char) 27 + "[31mWarning! " + (char)27 + "[0m");
+    }
+}
